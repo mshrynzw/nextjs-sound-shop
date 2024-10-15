2ed556e6d8a7b57c110017f886e77690aea9ed2e
mshrynzw <iq87io25@gmail.com>
2024-10-16 02:08:45 +0900

    Fix

--- components/home/Button.tsx
+++ components/home/Button.tsx
@@ -55,7 +55,7 @@ const Button = () => {
         className="z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold uppercase text-black transition pointer hover:bg-white/10 hover:text-white md:mt-4"
         onClick={handleCheckout}
       >
-        Checkout
+        Cart
         <span className="mx-1 rounded bg-blue-500 px-2 py-1 text-white">
           $ {isClient ? cart.length : 0}
         </span>
