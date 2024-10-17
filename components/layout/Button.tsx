ea76cec0c434a7971166d06419ed910e8ded3224
mshrynzw <iq87io25@gmail.com>
2024-10-17 20:18:53 +0900

    Fix

--- components/layout/Button.tsx
+++ components/layout/Button.tsx
@@ -4,9 +4,11 @@ import React, {useState, useEffect} from "react"
 import {useSession, signIn, signOut} from "next-auth/react"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faGoogle} from "@fortawesome/free-brands-svg-icons"
+import {faFolderOpen} from "@fortawesome/free-regular-svg-icons"
 import {useCart} from "@/context/CartContext"
 import {useModal} from "@/context/ModalContext"
 import ModalCart from "@/components/modal/ModalCart"
+import ModalOrder from "@/components/modal/ModalOrder"
 
 const Button = () => {
   const {cart} = useCart()
@@ -27,45 +29,67 @@ const Button = () => {
     })
   }
 
+  const handleOrder = () => {
+    openModal({
+      title: "Order",
+      content: <ModalOrder/>
+    })
+  }
+
   return (
-    <div className="mt-4 space-x-4">
-      {session ? (
+    <div className="mt-4 space-y-4">
+      <div className="space-x-4">
+        {session ? (
+          <button
+            className="z-10 mt-6 rounded-lg border border-white bg-white/90 px-3 py-2 text-sm font-semibold text-black transition pointer hover:bg-white/10 hover:text-white md:mt-4"
+            onClick={() => signOut()}
+          >
+            <div className="flex items-center justify-between space-x-2">
+              <div>Logout</div>
+              <div><FontAwesomeIcon icon={faGoogle}/></div>
+            </div>
+          </button>
+        ) : (
+          <button
+            className="z-10 mt-6 rounded-lg border border-white bg-white/90 px-3 py-2 text-sm font-semibold text-black transition pointer hover:bg-white/10 hover:text-white md:mt-4"
+            onClick={() => signIn("google")}
+          >
+            <div className="flex items-center justify-between space-x-2">
+              <div>Login</div>
+              <div><FontAwesomeIcon icon={faGoogle}/></div>
+            </div>
+          </button>
+
+        )}
         <button
-          className="z-10 mt-6 rounded-lg border border-white bg-white/90 px-3 py-2 text-sm font-semibold text-black transition pointer hover:bg-white/10 hover:text-white md:mt-4"
-          onClick={() => signOut()}
+          className="z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold uppercase text-black transition pointer hover:bg-white/10 hover:text-white md:mt-4"
+          onClick={handleCheckout}
         >
           <div className="flex items-center justify-between space-x-2">
-            <div>Logout</div>
-            <div><FontAwesomeIcon icon={faGoogle}/></div>
-          </div>
-        </button>
-      ) : (
-        <button
-          className="z-10 mt-6 rounded-lg border border-white bg-white/90 px-3 py-2 text-sm font-semibold text-black transition pointer hover:bg-white/10 hover:text-white md:mt-4"
-          onClick={() => signIn("google")}
-        >
-          <div className="flex items-center justify-between space-x-2">
-            <div>Login</div>
-            <div><FontAwesomeIcon icon={faGoogle}/></div>
+            <div>Cart</div>
+            <div>$ {isClient ? cart.length : 0}</div>
           </div>
         </button>
 
-      )}
-      <button
-        className="z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold uppercase text-black transition pointer hover:bg-white/10 hover:text-white md:mt-4"
-        onClick={handleCheckout}
-      >
-        <div className="flex items-center justify-between space-x-2">
-          <div>Cart</div>
-          <div>$ {isClient ? cart.length : 0}</div>
+        <button
+          className="z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold uppercase text-black transition pointer hover:bg-white/10 hover:text-white md:mt-4"
+        >
+          Donate
+        </button>
+      </div>
+      {session && (
+        <div className="space-x-4">
+          <button
+            className="z-10 rounded-lg border border-white bg-white/60 px-3 py-2 text-sm font-semibold text-black transition pointer hover:bg-white/10 hover:text-white w-full"
+            onClick={handleOrder}
+          >
+            <div className="flex items-center justify-center space-x-2">
+              <div>Order</div>
+              <div><FontAwesomeIcon icon={faFolderOpen}/></div>
+            </div>
+          </button>
         </div>
-      </button>
-
-      <button
-        className="z-10 mt-6 rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold uppercase text-black transition pointer hover:bg-white/10 hover:text-white md:mt-4"
-      >
-        Donate
-      </button>
+      )}
     </div>
   )
 }
--- components/modal/ModalOrder.tsx
+++ components/modal/ModalOrder.tsx
@@ -1,8 +1,9 @@
 "use client"
 
-import {useEffect, useState} from "react"
+import React, {useEffect, useState} from "react"
 import {supabase} from "@/lib/supabaseClient"
 import {formatLocalDate, formatLocalDateDeadline, isDeadline} from "@/lib/datetime"
+import {useModal} from "@/context/ModalContext"
 
 type OrderItem = {
   id: number;
@@ -15,6 +16,8 @@ type OrderItem = {
 }
 
 const ModalOrder = () => {
+  const {closeModal} = useModal()
+
   const [orders, setOrders] = useState<OrderItem[]>([])
   const [error, setError] = useState<string | null>(null)
 
@@ -49,84 +52,94 @@ const ModalOrder = () => {
   }
 
   return (
-    <div className="rounded bg-white bg-opacity-50 p-5">
-      <div className="min-w-full overflow-x-auto">
-        <table className="min-w-full align-middle text-sm">
-          <thead>
-          <tr className="border-b-2 border-neutral-100">
-            <th
-              className="px-3 py-2 text-center text-sm font-semibold uppercase tracking-wider text-neutral-700 min-w-[140px]"
-            >
-              ID
-            </th>
-            <th
-              className="px-3 py-2 text-center text-sm font-semibold uppercase tracking-wider text-neutral-700 min-w-[140px]"
-            >
-              Date
-            </th>
-            <th
-              className="px-3 py-2 text-center text-sm font-semibold uppercase tracking-wider text-neutral-700 min-w-[140px]"
-            >
-              Item
-            </th>
-            <th
-              className="px-3 py-2 text-center text-sm font-semibold uppercase tracking-wider text-neutral-700 min-w-[140px]"
-            >
-              Price
-            </th>
-            <th
-              className="px-3 py-2 text-center text-sm font-semibold uppercase tracking-wider text-neutral-700 min-w-[140px]"
-            >
-              Deadline
-            </th>
-            <th></th>
-          </tr>
-          </thead>
-          <tbody>
-          {orders.map(order => (
-            <tr key={order.id} className="border-b border-neutral-100 hover:bg-neutral-50">
-              <td className="p-3 text-center font-semibold text-neutral-600">
-                {order.id}
-              </td>
-              <td className="p-3 text-start text-neutral-600">
-                {formatLocalDate(order.created_at)}
-              </td>
-              <td className="p-3 text-start text-black">
-                {order.items.title}
-              </td>
-              <td className="p-3 font-medium text-neutral-600">
-                <div
-                  className="underline decoration-neutral-200 decoration-2 underline-offset-4 hover:text-neutral-950 hover:decoration-neutral-300">
-                  $ {order.items.price}
-                </div>
-              </td>
-              <td className="p-3 text-start text-neutral-600">
-                {isDeadline(order.created_at) ? (
-                  <div
-                    className="inline-block rounded-full bg-emerald-100 px-2 py-1 text-xs font-semibold leading-4 text-emerald-800"
-                  >
-                    {formatLocalDateDeadline(order.created_at)}
-                  </div>
-                ) : (
-                  <div
-                    className="inline-block whitespace-nowrap rounded-full bg-orange-100 px-2 py-1 text-xs font-semibold leading-4 text-orange-800"
-                  >
-                    {formatLocalDateDeadline(order.created_at)}
-                  </div>
-                )}
-              </td>
-              <td className="text-center font-medium ppy-3 ps-3">
-                {isDeadline(order.created_at) && (
-                  <button
-                    className="inline-flex items-center gap-1 rounded-lg border border-slate-200 font-medium text-slate-800 group px-2.5 py-1.5 hover:border-emerald-100 hover:bg-emerald-100 hover:text-emerald-800 active:border-slate-200">
-                    Download
-                  </button>
-                )}
-              </td>
+    <div className="space-y-4">
+      <div className="rounded bg-white bg-opacity-50 p-5">
+        <div className="min-w-full overflow-x-auto">
+          <table className="min-w-full align-middle text-sm">
+            <thead>
+            <tr className="border-b-2 border-neutral-100">
+              <th
+                className="px-3 py-2 text-center text-sm font-semibold uppercase tracking-wider text-neutral-700 min-w-[140px]"
+              >
+                ID
+              </th>
+              <th
+                className="px-3 py-2 text-center text-sm font-semibold uppercase tracking-wider text-neutral-700 min-w-[140px]"
+              >
+                Date
+              </th>
+              <th
+                className="px-3 py-2 text-center text-sm font-semibold uppercase tracking-wider text-neutral-700 min-w-[140px]"
+              >
+                Item
+              </th>
+              <th
+                className="px-3 py-2 text-center text-sm font-semibold uppercase tracking-wider text-neutral-700 min-w-[140px]"
+              >
+                Price
+              </th>
+              <th
+                className="px-3 py-2 text-center text-sm font-semibold uppercase tracking-wider text-neutral-700 min-w-[140px]"
+              >
+                Deadline
+              </th>
+              <th></th>
             </tr>
-          ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+            {orders.map(order => (
+              <tr key={order.id} className="border-b border-neutral-100 hover:bg-neutral-50">
+                <td className="p-3 text-center font-semibold text-neutral-600">
+                  {order.id}
+                </td>
+                <td className="p-3 text-start text-neutral-600">
+                  {formatLocalDate(order.created_at)}
+                </td>
+                <td className="p-3 text-start text-black">
+                  {order.items.title}
+                </td>
+                <td className="p-3 font-medium text-neutral-600">
+                  <div
+                    className="underline decoration-neutral-200 decoration-2 underline-offset-4 hover:text-neutral-950 hover:decoration-neutral-300">
+                    $ {order.items.price}
+                  </div>
+                </td>
+                <td className="p-3 text-start text-neutral-600">
+                  {isDeadline(order.created_at) ? (
+                    <div
+                      className="inline-block rounded-full bg-emerald-100 px-2 py-1 text-xs font-semibold leading-4 text-emerald-800"
+                    >
+                      {formatLocalDateDeadline(order.created_at)}
+                    </div>
+                  ) : (
+                    <div
+                      className="inline-block whitespace-nowrap rounded-full bg-orange-100 px-2 py-1 text-xs font-semibold leading-4 text-orange-800"
+                    >
+                      {formatLocalDateDeadline(order.created_at)}
+                    </div>
+                  )}
+                </td>
+                <td className="text-center font-medium ppy-3 ps-3">
+                  {isDeadline(order.created_at) && (
+                    <button
+                      className="inline-flex items-center gap-1 rounded-lg border border-slate-200 font-medium text-slate-800 group px-2.5 py-1.5 hover:border-emerald-100 hover:bg-emerald-100 hover:text-emerald-800 active:border-slate-200">
+                      Download
+                    </button>
+                  )}
+                </td>
+              </tr>
+            ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="space-x-4">
+        <button
+          onClick={closeModal}
+          className="rounded-lg border border-white bg-white px-3 py-2 text-sm font-semibold uppercase text-black shadow-2xl transition pointer hover:bg-white/10 hover:text-white"
+        >
+          Back
+        </button>
       </div>
     </div>
   )
