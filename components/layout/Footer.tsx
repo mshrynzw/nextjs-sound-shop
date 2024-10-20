a88b22c5b7a943a6744b1a3004720c6d5d2cfb41
mshrynzw <iq87io25@gmail.com>
2024-10-20 16:36:41 +0900

    Add: Link

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,14 +1,40 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faFacebook, faTwitter, faYoutube} from "@fortawesome/free-brands-svg-icons"
+import Link from "next/link"
 
 const Footer = () => {
   return (
     <>
       <div className="flex items-center space-x-2">
-        <div className="text-white/50">kaoruyukisound.com</div>
-        <div className="rounded bg-blue-500 px-1"><FontAwesomeIcon icon={faTwitter}/></div>
-        <div className="rounded bg-blue-700 px-1"><FontAwesomeIcon icon={faFacebook}/></div>
-        <div className="rounded bg-red-500 px-1"><FontAwesomeIcon icon={faYoutube}/></div>
+        <Link
+          href={process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000"}
+          className="text-white/50"
+        >
+          kaoruyukisound.com
+        </Link>
+        <Link
+          href={process.env.NEXT_PUBLIC_TWITTER_URL || "http://localhost:3000"}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="rounded bg-blue-500 px-1"
+        >
+          <FontAwesomeIcon icon={faTwitter}/>
+        </Link>
+        <Link
+          href={process.env.NEXT_PUBLIC_YOUTUBE_URL || "http://localhost"}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="rounded bg-red-500 px-1"
+        >
+          <FontAwesomeIcon icon={faYoutube}/>
+        </Link>
+        <Link
+          href={process.env.NEXT_PUBLIC_FACEBOOK_URL || "http://localhost"}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="rounded bg-blue-700 px-1">
+          <FontAwesomeIcon icon={faFacebook}/>
+        </Link>
       </div>
       <div>Copyright © {new Date().getFullYear()} Kaoru Yuki</div>
     </>
